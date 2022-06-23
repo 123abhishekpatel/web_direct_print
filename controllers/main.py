b67c2ee14dee600147dc997f8ca16390a44b2e3c
Abhishek Patel <38226152+123abhishekpatel@users.noreply.github.com>
2022-06-24 00:38:56 +0530

    Update main.py

--- controllers/main.py
+++ controllers/main.py
@@ -1,12 +1,12 @@
 from odoo import http
 from odoo.http import request
-
+import json
 
 class WebDirectPrintController(http.Controller):
 
     @http.route('/web_direct_print/data', auth='user')
-    def web_direct_print_data(self):
+    def web_direct_print_data(self,**kw):
         report_list = request.env['ir.actions.report'].sudo().search([['is_print','=',True]]).read(['report_name'])
         if report_list:
-            return list(map(lambda x: x['report_name'],report_list[0]))
-        return []
+            return json.dumps(list(map(lambda x: x['report_name'],report_list[0])))
+        return json.dumps([])
